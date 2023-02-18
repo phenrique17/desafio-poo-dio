b6992521737a070f8f38e7e69c8e59e4f85415fb
Paulo Santos <phenriquesantos17@gmail.com>
2023-02-17 23:05:35 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/desafiodiopoo/DesafioDioPooApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafiodiopoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDioPooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioDioPooApplication.class, args);
+    }
+
+}
